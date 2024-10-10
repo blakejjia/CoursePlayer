e440d41be22c93c42a730dee8093b12e4cb6f391
blake <black_jia@outlook.com>
2024-10-10 14:03:39 +0800

    great demo for ls

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'dart:io';
 
 void main() {
   runApp(const MainApp());
@@ -10,9 +12,65 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+      home: FileListPage(),
+    );
+  }
+}
+
+class FileListPage extends StatefulWidget {
+  const FileListPage({super.key});
+
+  @override
+  _FileListPageState createState() => _FileListPageState();
+}
+
+class _FileListPageState extends State<FileListPage> {
+  String data = 'Loading...';
+
+  @override
+  void initState() {
+    super.initState();
+    requestStoragePermissionAndListFiles();
+  }
+
+  Future<void> requestStoragePermissionAndListFiles() async {
+    // 请求存储权限
+    var status = await Permission.storage.status;
+    if (!status.isGranted) {
+      await Permission.storage.request();
+    }
+
+    listFilesInDirectory();
+  }
+
+  void listFilesInDirectory() async {
+    final directory = Directory('/storage/emulated/0');
+    if (await directory.exists()) {
+      List<FileSystemEntity> files = directory.listSync();
+      setState(() {
+        data = files.isEmpty
+            ? 'No files found'
+            : files.map((file) => file.path).join('\n');
+      });
+    } else {
+      setState(() {
+        data = 'Directory does not exist';
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('File List')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('Trying to ls storage/emulated/0'),
+            const SizedBox(height: 20),
+            Text("Data found:\n$data"),
+          ],
         ),
       ),
     );
